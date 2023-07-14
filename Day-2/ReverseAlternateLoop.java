da9c70661551c456e207fd87f36a8c7dae713926
saiicharannnnnn <139194236+saiicharannnnnn@users.noreply.github.com>
2023-07-14 11:36:40 +0530

    Add files via upload

---
+++ Day-2/ReverseAlternateLoop.java
@@ -0,0 +1,10 @@
+public class ReverseAlternateLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=100;i>=0;i=i-3) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ Day-2/ReverseLoop.java
@@ -0,0 +1,10 @@
+public class ReverseLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=100;i>=0;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
